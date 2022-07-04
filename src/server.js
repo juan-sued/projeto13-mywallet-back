7fcaac0092a4f015dce458ade16010ca618da89d
juansued1233536 <92616927+juansued1233536@users.noreply.github.com>
2022-07-04 16:17:23 -0300

    bug server fixed

--- src/server.js
+++ src/server.js
@@ -7,6 +7,14 @@ import transationsRoutes from './routes/transactionsRoutes.js';
 const server = express();
 
 server.use(cors());
+
+server.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
+
 server.use(express.json());
 
 server.use(authRouter);
