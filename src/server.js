9a75e1ed829bc8db5aa717f344e16de5d1dabc57
juansued1233536 <92616927+juansued1233536@users.noreply.github.com>
2022-07-04 17:16:37 -0300

    bug server fixed

--- src/server.js
+++ src/server.js
@@ -8,13 +8,6 @@ const server = express();
 
 server.use(cors());
 
-server.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  next();
-});
-
 server.use(express.json());
 
 server.use(authRouter);
