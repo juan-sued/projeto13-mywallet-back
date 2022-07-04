b50d3c2e32ef59038d1ff670340b193d8e14983f
juansued1233536 <92616927+juansued1233536@users.noreply.github.com>
2022-07-04 17:30:39 -0300

    bug server fixed

--- src/server.js
+++ src/server.js
@@ -12,6 +12,15 @@ server.use(
   })
 );
 
+server.use((req, res, next) => {
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header('Access-Control-Allow-Origin', '*');
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  server.use(cors());
+  next();
+});
+
 server.use(express.json());
 
 server.use(authRouter);
