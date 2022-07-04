9476267286fe8fb9092d219672289a1bd07961de
juansued1233536 <92616927+juansued1233536@users.noreply.github.com>
2022-07-04 14:37:54 -0300

    bug server fixed

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,10 @@
 import { createUser, login } from '../controllers/Auth/authController.js';
 import { Router } from 'express';
-
+import cors from 'cors';
 import validateUser from '../middlewares/userMiddleware.js';
 
 const router = Router();
-
+server.use(cors());
 router.post('/sign-up', validateUser, createUser);
 
 router.post('/', login);
