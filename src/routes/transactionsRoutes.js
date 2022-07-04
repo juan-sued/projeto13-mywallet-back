15c4557addd2dce4ad9750af1fe251835dc7b919
juansued1233536 <92616927+juansued1233536@users.noreply.github.com>
2022-07-04 14:41:59 -0300

    bug server fixed

--- src/routes/transactionsRoutes.js
+++ src/routes/transactionsRoutes.js
@@ -2,13 +2,13 @@ import {
   registerTransactions,
   infosHomePage
 } from '../controllers/Transactions/transactionsController.js';
-import cors from 'cors';
+
 import { Router } from 'express';
 
 import authUser from '../middlewares/authMiddleware.js';
 
 const router = Router();
-server.use(cors());
+
 router.post('/transactions/:typeTransaction', authUser, registerTransactions);
 
 router.get('/home', authUser, infosHomePage);
--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import chalk from 'chalk';
-
+import cors from 'cors';
 import authRouter from './routes/authRoutes.js';
 import transationsRoutes from './routes/transactionsRoutes.js';
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 
 server.use(authRouter);
